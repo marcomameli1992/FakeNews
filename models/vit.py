15459ea8cdaa2c4f1e191fe2082f729563b66bfa
Marco Mameli <mameli.1.marco@gmail.com>
2022-01-03 12:34:39 +0100

    Change the datatype output
    This changes permit to obtain the output as PyTorch Tensor and not a BatchFeatures.

--- models/vit.py
+++ models/vit.py
@@ -8,11 +8,11 @@ class ViTFT(nn.Module):
         self.vit = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224-in21k')
 
     def forward(self, x):
-        return self.vit(x)
+        return self.vit(x, return_tensors='pt')['pixel_values']
 
 if __name__ == "__main__":
     import torch
     feature_extractor = ViTFT()
-    img_rand = torch.rand(3, 224, 224)
+    img_rand = torch.rand(3, 216, 216)
     img_feature = feature_extractor(img_rand)
 
