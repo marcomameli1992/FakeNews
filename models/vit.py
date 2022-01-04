85d38098a4cd66760d7b2b5c7c7274d3aa87c45b
Marco Mameli <mameli.1.marco@gmail.com>
2022-01-04 09:30:52 +0100

    Solved some problems
    The VIT model uses a custom output data type with this improvment the output was custed to PT Tensor.
    Moreover the output dimension was filled to 128 squared image

--- models/vit.py
+++ models/vit.py
@@ -5,14 +5,24 @@ class ViTFT(nn.Module):
 
     def __init__(self):
         super(ViTFT, self).__init__()
-        self.vit = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224-in21k')
+        self.vit = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224-in21k', size=128)
+        #self.conv = nn.Conv2d(3, 64, 3, )
 
     def forward(self, x):
-        return self.vit(x, return_tensors='pt')['pixel_values']
+        features = self.vit(x, return_tensors='pt')['pixel_values']
+
+        return features#self.conv(features)
 
 if __name__ == "__main__":
     import torch
+    from skimage.io import imread
+    from torchvision import transforms
     feature_extractor = ViTFT()
-    img_rand = torch.rand(3, 216, 216)
-    img_feature = feature_extractor(img_rand)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    feature_extractor.to(device)
+    img_rand = imread('/Users/marcomameli01/Downloads/Dataset/test/1a1dfb.jpg')
+    transform = transforms.ToTensor()
+    img_rand = transform(img_rand)
+    img_feature = feature_extractor(torch.unsqueeze(img_rand, dim=0))
 
