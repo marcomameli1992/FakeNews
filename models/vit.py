e334e846aa8f086d02be948b138d28ad88c0a07f
Marco Mameli <mameli.1.marco@gmail.com>
2022-01-05 12:54:24 +0100

    Test for the vit from timm

--- models/vit.py
+++ models/vit.py
@@ -17,13 +17,14 @@ class ViTFT(nn.Module):
 if __name__ == '__main__':
     import torch
     from skimage.io import imread
+    from skimage.transform import resize
     from torchvision import transforms
 
     feature_extractor = ViTFT()
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     feature_extractor.to(device)
-    img_rand = imread('/Users/marcomameli01/Downloads/Dataset/test/1a1dfb.jpg')
+    img_rand = resize(imread('/home/vrai/FakeNews2/Dataset/test/1a1dfb.jpg'), (224, 224, 3))
     transform = transforms.ToTensor()
     img_rand = transform(img_rand)
-    img_feature = feature_extractor(torch.unsqueeze(img_rand, dim=0))
+    img_feature = feature_extractor(torch.unsqueeze(img_rand.type(torch.float).to(device), dim=0))
