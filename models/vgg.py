2d9747d9bb4355f85995411593cf62f3b1d02dd0
Marco Mameli <mameli.1.marco@gmail.com>
2022-01-12 11:15:47 +0100

    Testing VGG

--- models/vgg.py
+++ models/vgg.py
@@ -11,3 +11,11 @@ class VGG16FT(nn.Module):
         output = self.vgg(x)
         features = self.vgg.features(x) #self.vgg.classifier[0](intermedia_out)
         return features, output
+
+if __name__ == '__main__':
+    import torch
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    feature_extractor = VGG16FT()
+    feature_extractor.to(device)
